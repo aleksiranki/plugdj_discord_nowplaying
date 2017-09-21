3e4acb7349708b702b6cbdb0862759c9874f1ad4
Aleksi Ranki <aleksi.ranki@iki.fi>
2017-09-21 18:30:39 +0300

    Added feature which changes the behaviour so that the bot changes the topic of discord channel to the song playing. Also added feature which bridges the messages between plug.dj and discord chats. It's a dirty hax for now (you need to specify the usernames in config to prevent loop) but will make it better later.

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # plugdj_discord_nowplaying
-A bot which prints current song playing in plug.dj channel to Discord. This is just a dirty hack and has lot's of issues but will do the trick. It uses discord.js to act as Discord bot and plugAPI to talk with plug.dj API.
+A bot which prints current song playing in plug.dj channel to Discord. This is just a dirty hack and has lot's of issues but will do the trick.
+
+Started just as a simple bot to display song playing in plug.dj to Discord but expanded also to be able bridge messages between discord and plug.dj chats.
+
+It uses discord.js to act as Discord bot and plugAPI to talk with plug.dj API.
 ## Installation and usage
 Before you can run this you need to create the config.json file for configuration.
 
@@ -21,9 +25,13 @@ Also you need plug.dj username and password to access the plug.dj API.
 {
    "discordbottoken": "token",
    "discordchannel": "channel",
+   "discordusername": "username", // Used for message briding to prevent loop
    "plugdjroomslug": "slug",
    "plugdjemail": "email",
-   "plugdjpassword": "password"
+   "plugdjpassword": "password",
+   "plugdjusername": "username", // Used for message bridging to prevent loop
+   "topicmode": "no",            // If you want the information about current song to be displayed in topic instead of message change this to yes
+   "bridgemessages": "no"        // Set to yes if you want to bridge messages between plug.dj chat and Discord chat. For now needs the discordusername and plugdjusername to be set.
 }
 ```
 Discord channel needs to be the channel ID format. Write `\#channelname` on the relevant server in Discord. It will produce something like `<#1234567890>`. The 1234567890 needs to be used in discordchannel.
--- index.js
+++ index.js
@@ -31,11 +31,11 @@ var reconnect = function() { bot.connect(config.plugdjroomslug); };
 bot.on('close', reconnect);
 bot.on('error', reconnect);
 
+// Find the channel in Discord
+var channel = client.channels.get(config.discordchannel);
+
 // Event listener for plug.dj
 bot.on('advance', function(data) {
-   // Find the channel in Discord
-   var channel = client.channels.get(config.discordchannel);
-
    // Do not do anything if no-one is playing
    if (typeof data.media == 'undefined')
    {
@@ -47,11 +47,58 @@ bot.on('advance', function(data) {
    var currentdj = data.currentDJ.username;
 
    // Create the message to display in Discord and send it to Discrod
-   var message = "**Current DJ:** " + currentdj + "\n" + songauthor + " - " + songtitle;
-   channel.send(message);
+   var message = songauthor + " - " + songtitle + "\n**DJ:** " + currentdj;
+   if (typeof config.topicmode == "string" && config.topicmode == "yes")
+   {
+      channel.setTopic(message, function(data) {
+         console.log("Tried to set topic");
+      });
+   }
+   else
+   {
+      channel.send(message);
+   }
 
    // Log stuff to JS console
-   console.log(data.mediaStartTime)
    console.log(message);
-   console.log("\n");
+   console.log("");
 });
+
+// Check if the message bridging is on
+if (typeof config.bridgemessages == "string" && config.bridgemessages == "yes")
+{
+   // Event listener for messages in plug.dj
+   bot.on('chat', function(data) {
+      var user = data.from.username;
+      // Prevent loop if message was sent by the bot
+      // TODO: Better way for this
+      if (user == config.plugdjusername)
+      {
+         return;
+      }
+      var plugdjmessage = data.message;
+      var message = "**<" + user + "**> " + plugdjmessage;
+      channel.send(message);
+      console.log("Plugdj: " + message);
+   });
+
+   // Event listener for messages in Discord channel
+   client.on('message', function(discordmessage) {
+      // Bridge messages only from the chosen channel
+      if (discordmessage.channel == channel)
+      {
+         var user = discordmessage.author.username;
+         // Prevent loop if message was sent by the bot
+         // TODO: Better way to do this
+         if (user == config.discordusername)
+         {
+            return;
+         }
+         var content = discordmessage.content;
+         var message = "<" + user + "> " + content;
+         bot.sendChat(message);
+         console.log("Discord: " + message);
+      }
+   });
+}
+
